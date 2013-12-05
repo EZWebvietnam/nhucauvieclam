c55cf93d92b7179f1193a8160471bf7a064ee27e
GiangBeo91 <nguyentruonggiang91@gmail.com>
2013-12-06 00:22:51 +0700

    add test file

---
+++ application/modules/home/controllers/test.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Test extends CI_Controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->load->library('controllerlist'); // Load the library
+        $data['_folder_rot']='nhucauvieclam';
+        $data['_module']='dangky';
+        $data['_rot']=$_SERVER['DOCUMENT_ROOT'];
+        print_r($this->controllerlist->initialize($data));
+        exit;
+    }
+}
+?>
