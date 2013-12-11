487ee5f470fdde70f5aa4d598b81de3dbb78eeef
Nguyen Truong Giang <nguyentruonggiang91@gmail.com>
2013-12-11 17:30:28 +0700

    edit config db

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'timvieclam';
+$db['default']['hostname'] = HOST;
+$db['default']['username'] = DB_USER;
+$db['default']['password'] = DB_PASS;
+$db['default']['database'] = DB_NAME;
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
