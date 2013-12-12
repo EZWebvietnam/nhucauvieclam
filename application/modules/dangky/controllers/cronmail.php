80472765c27ea6ae41286db2e2fa026b804383e2
Nguyen Truong Giang <nguyentruonggiang91@gmail.com>
2013-12-12 11:17:11 +0700

    cron mail to ung vien

--- application/modules/dangky/controllers/cronmail.php
+++ application/modules/dangky/controllers/cronmail.php
@@ -4,32 +4,56 @@ class Cronmail extends MY_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('session');
     }
     public function index()
     {
         $this->load->model('cronmailmodel');
         $list_user = $this->cronmailmodel->get_id_nghe();
-        $link = "";
+        $count = count($list_user);
+        $link="";
         foreach($list_user as $user)
         {
+            
             $list_job = $this->cronmailmodel->get_job($user['m_id']);
-            foreach($list_job as $job)
+            if($this->session->userdata('mail_user')==$user['u_email'])
             {
-                $link .="<a href='http://dev.nhucauvieclam.net/tin-tuyen-dung/".$job['e_id']."-".mb_strtolower(url_title(removesign($job['e_title'])))."'>".$job['e_title']."</a><br>";
+                foreach($list_job as $job)
+                {
+                    $link .="<a href='http://dev.nhucauvieclam.net/tin-tuyen-dung/".$job['e_id']."-".mb_strtolower(url_title(removesign($job['e_title'])))."'>".$job['e_title']."</a><br>";
+                }
             }
-            $this->sendmail($user['u_email'],$user['u_fullname'],$link);
-            $link="";
+            if($this->session->userdata('mail_user'))
+            {
+                if($this->session->userdata('mail_user')!=$user['u_email'])
+                {
+                    $this->sendmail($this->session->userdata('mail_user'),$link);
+                    $link="";
+                    foreach($list_job as $job)
+                    {
+                        $link .="<a href='http://dev.nhucauvieclam.net/tin-tuyen-dung/".$job['e_id']."-".mb_strtolower(url_title(removesign($job['e_title'])))."'>".$job['e_title']."</a><br>";
+                    }
+                    $this->sendmail($user['u_email'],$link);
+                    
+                }
+                
+            }
+            else
+            {
+                $this->session->set_userdata('mail_user',$user['u_email']);
+            }
+ 
         }
-        sleep(2);
     }
-    public function sendmail($to,$to_name,$link)
+    public function sendmail($to,$link)
     {
-        $this->load->library('mailer');
+        $this->load->library('maillinux');
         $subject="Danh sách việc làm mới nhất";
-        $content = "Xin chào bạn ".$to_name."<br>
+        $content = "Xin chào bạn 
         Nhu Cầu Việc Làm xin gửi bạn danh sách việc làm phù hợp với bạn nhất<br>
         ".$link;
-        $this->mailer->sendmail($to,$to_name,$subject,$content);
+        $from = "thangdq@nhucauvieclam.net";
+        $this->maillinux->SendMail($from,$to,$subject,$content);
     }
 }
 ?>
